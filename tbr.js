600067925fb784d77ece5818655b6bc5a1cf22b0
dantican <120753765+dantican@users.noreply.github.com>
2023-01-30 19:49:07 -0700

    Finished the view object. Added book locations and main model properties

--- tbr.js
+++ tbr.js
@@ -11,6 +11,11 @@
       }
     },
 
+    displayMessage: function(msg) {
+      var bottomBox = document.getElementById("bottom-box");
+      bottomBox.innerHTML = msg;
+    },
+
     displayBookHit: function(location) {
       var cell = document.getElementById(location);
       cell.setAttribute("class", "hit");
@@ -22,6 +27,28 @@
     }
   }
 
-view.displayBookHit("23");
-view.displayXMiss("14");
+var model = {
+  boardSize: 8,
+  numBooks: 5,
+  bookLength: 1,
+  booksFound: 0,
+
+  books: [{locations:["00", "23", "44"], hits:["", "", ""]},
+          {locations:["10", "33", "34"], hits:["", "", ""]},
+          {locations:["32", "46", "37"], hits:["", "", ""]},
+          {locations:["15", "16", "27"], hits:["", "", ""]},
+          {locations:["14", "26", "25"], hits:["", "", ""]}],
+
+  fire: function(guess) {
+    for (i = 0; i < this.numBooks; i++) {
+      var book = this.books[i];
+      var index = book.locations.indexOf(guess);
+      if (index >= 0) {
+        book.hits[index] = "hit";
+        view.displayBookHit(guess);
+        view.displayMessage("You hit a book. I wonder which one...")
+      }
+    }
+  },
+}
 
